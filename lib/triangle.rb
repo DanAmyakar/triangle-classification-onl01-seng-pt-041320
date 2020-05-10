0d261c4fc7d932e1a4d4a2c2519e3631e4dfc5f0
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-10 17:28:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,58 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle. The sum of any two sides of a triangle always exceeds the length of the third side. Triangle inequality principle."
+    end
+  end
+  
+  attr_reader :a, :b, :c
+  @@all = []
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def valid?
+    valid_flag = nil
+    if ((@a > 0) && (@b > 0) && (@c > 0) && ((@a + @b) > @c && (@b + @c) > @a && (@c + @a) > @b))
+      valid_flag = true
+    else
+      valid_flag = false
+    end
+  end
+  
+  def equilateral
+    :equilateral if (@a == @b && @a == @c)
+  end
+  
+  def isosceles
+    :isosceles if (((@a==@b) && (@a!=@c)) || ((@a==@c) && (@a!=@b)) || ((@b==@c) && (@b!=@a)))
+  end
+  
+  def scalene
+    :scalene
+  end
+  
+  def kind
+    if valid? == true
+      equilateral || isosceles || scalene
+    else valid? == false
+      raise TriangleError
+    end
+  end
+  
 end
