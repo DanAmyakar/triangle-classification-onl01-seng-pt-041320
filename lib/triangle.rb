9ce13485ff5093567a20dd241cefefb44cfdd0c0
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-10 14:33:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,67 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c
+  @@all = []
+  
+  def initialize(a, b, c)
+    @side_a = a
+    @side_b = b
+    @side_c = c
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def group
+    group = [@side_a, @side_b, @side_c]
+  end
+  
+  def valid?
+    valid_flag = false
+    if ((@side_a > 0 && @side_b > 0 && @side_c > 0) && (@side_a + @side_b > @side_c || @side_a + @side_c > @side_b || @side_b + @side_c > @side_a))
+      valid_flag = true
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+    valid_flag
+  end
+  
+  def equilateral
+    :equilateral if ( @side_a == @side_b && @side_a == @side_c)
+  end
+  
+  def isosceles
+    :isosceles if (@side_a == @side_b || @side_a == @side_c || @side_b == @side_c)
+  end
+  
+  def scalene
+    group.collect.sort {|a, b| a <=> b}
+    :scalene if (group[0] > group[1] && group[0] != group[1] && group[1] != group[2]) 
+  end
+  
+  def kind
+    valid?
+    equilateral || isosceles || scalene
+  end
+  
+  
+  
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle. The sum of any two sides of a triangle always exceeds the length of the third side. Triangle inequality principle."
+    end
+  end
+  
 end
